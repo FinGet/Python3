8108568721a871ecf641578a94d490e4040c7d60
“FinGne <zhangbiaoFT@163.com>
2018-12-17 18:01:08 +0800

    demo

---
+++ demo1.py
@@ -0,0 +1,27 @@
+""" 逻辑语句 """
+# if else 
+mood = True
+if mood :
+  print('gogogo')
+
+# 代码不能压缩
+
+# input 得到的是一个字符串，所以需要转换类型
+num = int(input('请输出一个数字：'))
+
+if num > 5: 
+  print('大于5')
+else:
+  print('小于5')
+
+if num:
+  pass
+else:
+  pass
+
+if num > 5:
+  pass
+elif num > 10:
+  pass
+else:
+  pass
---
+++ hello.py
@@ -0,0 +1,5 @@
+# 常量大写
+PI = '3.14159'
+
+hello = "hello word"
+print(hello)
